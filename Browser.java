7004c0b25f8d2aa47f9337895bf10d4ea0dd88d6
Vinothkumar-SV <95397492+Vinothkumar-SV@users.noreply.github.com>
2022-10-11 10:57:05 +0530

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,15 @@
+package week1day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chrome brow = new Chrome();
+		brow.printName();
+		brow.getName();
+
+		
+	}
+	
+
+}
---
+++ Mobile.java
@@ -0,0 +1,9 @@
+package wek3day2;
+
+public abstract class Mobile {
+
+	public abstract void samsung(); 
+	public void motorola();{
+}
+}
+
